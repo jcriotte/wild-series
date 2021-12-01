505eddc301a77eea0aee7b0c8c5091fa251b9aa1
RIOTTE <jcriotte69@gmail.com>
2021-12-01 15:50:33 +0100

    after quete12

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -7,12 +7,43 @@ use App\Entity\Program;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Form\CategoryType;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * @Route("/category", name = "category_")
  */
 class CategoryController extends AbstractController
 {
+    /**
+     * The controller for the category add form
+     *
+     * @Route("/new", name="new")
+     */
+    public function new(Request $request): Response
+    {
+        // Create a new Category Object
+        $category = new Category();
+        // Create the associated Form
+        $form = $this->createForm(CategoryType::class, $category);
+        // Get data from HTTP request
+        $form->handleRequest($request);
+        // Was the form submitted ?
+        if ($form->isSubmitted()) {
+            // Deal with the submitted data
+            // Get the Entity Manager
+            $entityManager = $this->getDoctrine()->getManager();
+            // Persist Category Object
+            $entityManager->persist($category);
+            // Flush the persisted object
+            $entityManager->flush();
+            // Finally redirect to categories list
+            return $this->redirectToRoute('category_index');
+        }
+        // Render the form
+        return $this->render('category/new.html.twig', ["form" => $form->createView()]);
+    }
+
     /**
      * Show all rows from Category’s entity
      * 
@@ -45,11 +76,14 @@ class CategoryController extends AbstractController
                 'No category with name : ' . $categoryName . ' found in category\'s table.'
             );
         }
-        
+
         $programs = $this->getDoctrine()
             ->getRepository(Program::class)
-            ->findBy(['category' => $category->getId()],
-            ['id' => 'ASC'],3);
+            ->findBy(
+                ['category' => $category->getId()],
+                ['id' => 'ASC'],
+                3
+            );
 
         return $this->render('Category/show.html.twig', [
             'category' => $category, 'programs' => $programs,
